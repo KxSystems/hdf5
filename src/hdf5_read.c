0f6c13e924fe00665fc7725820fec6a298fdae3d
cmccarthy1 <38653604+cmccarthy1@users.noreply.github.com>
2020-04-03 16:53:36 +0100

    Compound data, file size and fixed length strings (#8)
    * Addition of support for unenlisted numeric ana atomic entries
    
    * handling of char arrays
    
    * Support for file size checking MB, simple compound datasets and fixed length string data

--- src/hdf5_read.c
+++ src/hdf5_read.c
@@ -14,6 +14,12 @@ K readLong(hid_t dset, char *rdtyp);
 K readReal(hid_t dset, char *rdtyp);
 K readShort(hid_t dset, char *rdtyp);
 K readChar(hid_t dset, char *rdtyp);
+K readCompound(hid_t dset, char *rdtyp);
+K compoundInteger(hid_t dset, char* memb_name, hsize_t dims, hid_t item_type,char *rdtyp);
+K compoundFloat(hid_t dset, char* memb_name, hsize_t dims, hid_t item_type,char *rdtyp);
+K compoundString(hid_t dset, char* memb_name, hsize_t dims, hid_t item_type,char *rdtyp);
+
+
 
 // Read data from an attribute associated with a group or dataset
 EXP K hdf5readAttrDataset(K fname, K dname, K aname){
@@ -105,18 +111,24 @@ K readData(hid_t data,char *rdtyp){
   else if(strcmp("a",rdtyp)==0)
     dtype = H5Aget_type(data);
   ntype = H5Tget_native_type(dtype,H5T_DIR_ASCEND);
-  if(H5Tequal(ntype, HDF5INT))
-    result = readInt(data,rdtyp);
-  else if(H5Tequal(ntype, HDF5LONG))
-    result = readLong(data,rdtyp);
-  else if(H5Tequal(ntype, HDF5FLOAT))
-    result = readFloat(data,rdtyp);
-  else if(H5Tequal(ntype, HDF5REAL))
-    result = readReal(data,rdtyp);
-  else if(H5Tequal(ntype, HDF5SHORT))
-    result = readShort(data,rdtyp);
-  else 
-    result = readChar(data, rdtyp);
+  // Read as a compound datatype if suitable
+  if(H5T_COMPOUND == H5Tget_class(dtype))
+    result = readCompound(data,rdtyp);
+  // Read vanilla datatypes
+  else{
+    if(H5Tequal(ntype, HDF5INT))
+      result = readInt(data,rdtyp);
+    else if(H5Tequal(ntype, HDF5LONG))
+      result = readLong(data,rdtyp);
+    else if(H5Tequal(ntype, HDF5FLOAT))
+      result = readFloat(data,rdtyp);
+    else if(H5Tequal(ntype, HDF5REAL))
+      result = readReal(data,rdtyp);
+    else if(H5Tequal(ntype, HDF5SHORT))
+      result = readShort(data,rdtyp);
+    else 
+      result = readChar(data, rdtyp);
+  }
   H5Tclose(dtype);
   H5Tclose(ntype);
   return result;
@@ -262,46 +274,219 @@ K readShort(hid_t dset,char *rdtyp){
   return(z);
 }
 
-// Read character datasets 
+// Read character datasets
 K readChar(hid_t dset, char *rdtyp){
-  hid_t space, memtype;
+  hid_t space, memtype,dtype;
   K vla;
-  int i;
   hsize_t dims[1];
   char **rdata;
   // memory type is standard C string
   memtype = H5Tcopy(H5T_C_S1);
-  // data can be of variable length
-  H5Tset_size(memtype, H5T_VARIABLE);
-  // Deal with data associated with attribute
-  if(strcmp(rdtyp,"a")==0){
-    // get the dataspace and retrieve dimensionality
-    space = H5Aget_space(dset);
-    H5Sget_simple_extent_dims(space, dims, NULL);
-    // Allocate memory to write to
-    rdata = (char **)malloc(dims[0] * sizeof(char *));
-    H5Aread(dset, memtype, rdata);
-  }
-  // Deal with data assoicated with dataset
-  else{
+
+  if(strcmp("d",rdtyp)==0){
+    dtype = H5Dget_type(dset);
     space = H5Dget_space(dset);
-    H5Sget_simple_extent_dims(space, dims, NULL);
-    rdata = (char **)malloc(dims[0] * sizeof(char *));
-    H5Dread(dset,memtype, H5S_ALL, H5S_ALL, H5P_DEFAULT,rdata);
+  }
+  else{
+    dtype = H5Aget_type(dset);
+    space = H5Aget_space(dset);
+  }
+  // Assign memory for char arrays
+  H5Sget_simple_extent_dims(space, dims, NULL);
+  rdata = (char **)malloc(dims[0] * sizeof(char *));
+
+  if(H5Tis_variable_str(dtype)){       
+    H5Tset_size(memtype, H5T_VARIABLE);
+    if(strcmp(rdtyp,"a")==0)
+      H5Aread(dset, memtype, rdata);
+    else
+      H5Dread(dset,memtype, H5S_ALL, H5S_ALL, H5P_DEFAULT,rdata);
+  }
+  else{
+    size_t sz = H5Tget_size(dtype);
+    sz++;
+    H5Tset_size(memtype,sz);
+    rdata[0] = (char *)malloc(dims[0] * sz * sizeof(char));
+    for (int j=1; j<dims[0]; j++)
+      rdata[j] = rdata[0] + j * sz;
+    if(strcmp(rdtyp,"a")==0)
+      H5Aread(dset, memtype, rdata[0]);
+    else
+      H5Dread(dset, memtype, H5S_ALL, H5S_ALL, H5P_DEFAULT, rdata[0]);
   }
   // Write the read data to a K object either a char array or list
   vla = knk(0);
-  if(1==dims[0])
-   jk(&vla, kp(rdata[0]));
-  else{
-    for (i=0; i<dims[0]; i++)
-      jk(&vla, kp(rdata[i]));
-  }
+  for (int i=0; i<dims[0]; i++)
+    jk(&vla, kp((char*)rdata[i]));
   // Clean up
   // Reclaim unused memory from variable length data
-  H5Dvlen_reclaim(memtype, space, H5P_DEFAULT, rdata);
+  if(H5Tis_variable_str(dtype))
+    H5Dvlen_reclaim(memtype, space, H5P_DEFAULT, rdata);
   free(rdata);
   H5Sclose(space);
   H5Tclose(memtype);
   return vla;
+}
+
+K readCompound(hid_t dset, char *rdtyp){
+  int i, nmembs, rank;
+  hid_t dtype, space;
+  hid_t ntype, item_type;
+  H5T_class_t memb_cls;
+  dtype  = H5Dget_type(dset);
+  ntype  = H5Tget_native_type(dtype, H5T_DIR_ASCEND);
+  K key_vals, data_vals, memb_val;
+  data_vals = knk(0);
+  if(strcmp(rdtyp,"a")==0)
+    space  = H5Aget_space(dset);
+  else
+    space  = H5Dget_space(dset);
+  rank   = H5Sget_simple_extent_ndims(space);
+  hsize_t dims[rank];
+  H5Sget_simple_extent_dims (space, dims, NULL);
+  nmembs      = H5Tget_nmembers(ntype);
+  key_vals    = ktn(KS, nmembs);
+  for(i = 0;i < nmembs; i++){
+    char* member_name = H5Tget_member_name(ntype,i);
+    kS(key_vals)[i] = member_name;
+    memb_cls  = H5Tget_member_class(ntype,i);
+    item_type = H5Tget_member_type(dtype, i);
+    if(memb_cls == H5T_INTEGER)
+      memb_val = compoundInteger(dset, member_name, dims[0], item_type,rdtyp);
+    else if(memb_cls == H5T_FLOAT)
+      memb_val =   compoundFloat(dset, member_name, dims[0], item_type, rdtyp);
+    else if(memb_cls == H5T_STRING)
+      memb_val =  compoundString(dset, member_name, dims[0], item_type, rdtyp);
+    jk(&data_vals,memb_val);
+  }
+  H5Tclose(dtype);
+  H5Tclose(ntype);
+  H5Tclose(item_type);
+  H5Sclose(space);
+  return(xT(xD(key_vals,data_vals)));
+}
+
+
+K compoundInteger(hid_t dset, char* memb_name, hsize_t dims, hid_t item_type,char *rdtyp){
+  hid_t cmpd_dset;
+  int j;
+  K array;
+  if(H5Tequal(item_type,HDF5INT)){
+    int *rdata;
+    cmpd_dset = H5Tcreate(H5T_COMPOUND, sizeof(int));
+    H5Tinsert(cmpd_dset, memb_name, 0, item_type);
+    rdata = (int*)malloc(dims * sizeof(int));
+    if(strcmp(rdtyp,"a")==0)
+      H5Aread(dset, cmpd_dset, rdata);
+    else
+      H5Dread(dset, cmpd_dset, H5S_ALL, H5S_ALL, H5P_DEFAULT, rdata);
+    array = ktn(KI, dims);
+    for(int j = 0; j < dims; j++)
+      kI(array)[j] = *(rdata + j);
+    free(rdata);
+  }
+  else if(H5Tequal(item_type,HDF5LONG)){
+    long *rdata;
+    cmpd_dset = H5Tcreate(H5T_COMPOUND, sizeof(long));
+    H5Tinsert(cmpd_dset, memb_name, 0, item_type);
+    rdata = (long*)malloc(dims * sizeof(long));
+    if(strcmp(rdtyp,"a")==0)
+      H5Aread(dset, cmpd_dset, rdata);
+    else
+      H5Dread(dset, cmpd_dset, H5S_ALL, H5S_ALL, H5P_DEFAULT, rdata);
+    array = ktn(KJ, dims);
+    for(int j = 0; j < dims; j++)
+      kJ(array)[j] = *(rdata + j);
+    free(rdata);
+  }
+  else if(H5Tequal(item_type,HDF5SHORT)){
+    short *rdata;
+    cmpd_dset = H5Tcreate(H5T_COMPOUND, sizeof(short));
+    H5Tinsert(cmpd_dset, memb_name, 0, item_type);
+    rdata = (short*)malloc(dims * sizeof(short));
+    if(strcmp(rdtyp,"a")==0)
+      H5Aread(dset, cmpd_dset, rdata);
+    else
+      H5Dread(dset, cmpd_dset, H5S_ALL, H5S_ALL, H5P_DEFAULT, rdata);
+    array = ktn(KH, dims);
+    for(int j = 0; j < dims; j++)
+      kH(array)[j] = *(rdata + j);
+    free(rdata);
+  }
+  H5Tclose(cmpd_dset);
+  return array;
+}
+
+K compoundFloat(hid_t dset, char* memb_name, hsize_t dims, hid_t item_type,char *rdtyp){
+  hid_t cmpd_dset;
+  int j;
+  K array;
+  if(H5Tequal(item_type,HDF5FLOAT)){
+    double *rdata; 
+    cmpd_dset = H5Tcreate(H5T_COMPOUND, sizeof(double));
+    H5Tinsert(cmpd_dset, memb_name, 0, item_type);
+    rdata = (double*)malloc(dims * sizeof(double));
+    if(strcmp(rdtyp,"a")==0)
+      H5Aread(dset, cmpd_dset, rdata);
+    else
+      H5Dread(dset, cmpd_dset, H5S_ALL, H5S_ALL, H5P_DEFAULT, rdata);
+    array = ktn(KF, dims);
+    for(int j = 0; j < dims; j++)
+      kF(array)[j] = *(rdata + j);
+    free(rdata);
+  }
+  else if(H5Tequal(item_type,HDF5REAL)){
+    float *rdata;
+    cmpd_dset = H5Tcreate(H5T_COMPOUND, sizeof(float));
+    H5Tinsert(cmpd_dset, memb_name, 0, item_type);
+    rdata = (float*)malloc(dims * sizeof(float));
+    if(strcmp(rdtyp,"a")==0)
+      H5Aread(dset, cmpd_dset, rdata);
+    else
+      H5Dread(dset, cmpd_dset, H5S_ALL, H5S_ALL, H5P_DEFAULT, rdata);
+    array = ktn(KE, dims);
+    for(int j = 0; j < dims; j++)
+      kE(array)[j] = *(rdata + j);
+    free(rdata);
+  }
+  H5Tclose(cmpd_dset);
+  return array;
+}
+
+K compoundString(hid_t dset, char* memb_name, hsize_t dims, hid_t item_type,char *rdtyp){
+  hid_t cmpd_dset, temp_type;
+  int j;
+  K array = knk(0);
+  char **rdata;
+  rdata = (char **)malloc(dims * sizeof(char *));
+  temp_type = H5Tcopy(H5T_C_S1);
+  if(H5Tis_variable_str(item_type)){
+    H5Tset_size(temp_type, H5T_VARIABLE);
+    cmpd_dset = H5Tcreate(H5T_COMPOUND, sizeof(char *));
+    H5Tinsert(cmpd_dset, memb_name, 0, temp_type);
+    if(strcmp(rdtyp,"a")==0)
+      H5Aread(dset, cmpd_dset, rdata);
+    else
+      H5Dread(dset, cmpd_dset, H5S_ALL, H5S_ALL, H5P_DEFAULT, rdata);
+  }
+  else{
+    size_t sz = H5Tget_size(item_type);
+    sz++;
+    H5Tset_size(temp_type, sz);
+    cmpd_dset = H5Tcreate(H5T_COMPOUND, sz);
+    H5Tinsert(cmpd_dset, memb_name, 0, temp_type);
+    rdata[0] = (char *)malloc(dims * sz * sizeof(char));
+    for (int j=1; j<dims; j++)
+      rdata[j] = rdata[0] + j * sz;
+    if(strcmp(rdtyp,"a")==0)
+      H5Aread(dset,cmpd_dset,rdata[0]);
+    else
+      H5Dread(dset, cmpd_dset, H5S_ALL, H5S_ALL, H5P_DEFAULT, rdata[0]);
+  }
+  for(int j=0; j<dims; j++)
+    jk(&array,kp((char*)rdata[j]));
+  free(rdata);
+  H5Tclose(temp_type);
+  H5Tclose(cmpd_dset);
+  return array;
 }
