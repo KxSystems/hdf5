5a11d12b87bfa2602ff0f628cf1432a08abd5c5f
awilson-kx <awilson@kx.com>
2020-04-28 23:03:06 +0100

    move declarations to start

--- src/hdf5_create.c
+++ src/hdf5_create.c
@@ -8,7 +8,8 @@
 EXP K hdf5createFile(K fname){
   if(!kdbCheckType("[Cs]", fname))
     return KNL;
-  char *filename = kdbGetString(fname);
+  char *filename;
+  filename = kdbGetString(fname);
   H5Fcreate(filename, H5F_ACC_EXCL, H5P_DEFAULT, H5P_DEFAULT);
   // clean up
   free(filename);
@@ -20,7 +21,8 @@ EXP K hdf5createGroup(K fname, K gname){
     return KNL;
   hid_t file, group;
   hid_t gcpl; // group creation property list
-  char *filename   = kdbGetString(fname);
+  char *filename, *groupnames;
+  filename = kdbGetString(fname);
   htri_t filechk = H5Fis_hdf5(filename);
   if(filechk < 0)
     H5Fcreate(filename, H5F_ACC_EXCL, H5P_DEFAULT, H5P_DEFAULT);
@@ -29,7 +31,7 @@ EXP K hdf5createGroup(K fname, K gname){
     return krr((S)"file is not hdf5");
   }
   file = H5Fopen(filename, H5F_ACC_RDWR, H5P_DEFAULT);
-  char *groupnames = kdbGetString(gname);
+  groupnames = kdbGetString(gname);
   gcpl = H5Pcreate(H5P_LINK_CREATE);
   H5Pset_create_intermediate_group(gcpl, 1); // create intermediate groups
   group = H5Gcreate(file, groupnames, gcpl, H5P_DEFAULT, H5P_DEFAULT);
@@ -49,11 +51,12 @@ EXP K hdf5createDataset(K fname, K dname, K kdims, K ktype){
     return KNL;
   hid_t file;
   ktype_t dtype;
-  char *filename = kdbGetString(fname);
-  char *dataname = kdbGetString(dname);
+  char *filename, *dataname;
+  filename = kdbGetString(fname);
   if(H5Fis_hdf5(filename) <= 0)
     H5Fcreate(filename, H5F_ACC_EXCL, H5P_DEFAULT, H5P_DEFAULT);
   file = H5Fopen(filename, H5F_ACC_RDWR, H5P_DEFAULT);
+  dataname = kdbGetString(dname);
   dtype = getKType(ktype->g);
   if(dtype == NUMERIC)
     createNumericDataset(file, dataname, kdims, ktype);
@@ -71,13 +74,14 @@ EXP K hdf5createAttr(K fname, K dname, K aname, K kdims, K ktype){
     return KNL;
   hid_t file, data;
   ktype_t  dtype;
-  char *filename = kdbGetString(fname);
+  char *filename, *dataname, *attrname;
+  filename = kdbGetString(fname);
   file = H5Fopen(filename, H5F_ACC_RDWR, H5P_DEFAULT);
   if(file < 0){
     free(filename);
     return krr((S)"file does not exist");
   }
-  char *dataname = kdbGetString(dname);
+  dataname = kdbGetString(dname);
   data = openGroupData(file, dataname);
   if(data < 0){
     free(filename);
@@ -85,7 +89,7 @@ EXP K hdf5createAttr(K fname, K dname, K aname, K kdims, K ktype){
     H5Fclose(file);
     return krr((S)"group/dataset does not exist");
   }
-  char *attrname = kdbGetString(aname);
+  attrname = kdbGetString(aname);
   if(H5Aexists(data, attrname) > 0){
     closeGroupData(file, dataname, data);
     H5Fclose(file);
