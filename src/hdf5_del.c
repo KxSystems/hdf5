38ad437e567f6c7a8d295aadf4856549c94c820a
awilson-kx <awilson@kx.com>
2020-04-28 22:52:08 +0100

    JIT on getString

--- src/hdf5_del.c
+++ src/hdf5_del.c
@@ -9,22 +9,21 @@ EXP K hdf5delAttr(K fname, K dname, K aname){
     return KNL;
   hid_t file, data;
   char *filename = kdbGetString(fname);
-  char *dataname = kdbGetString(dname);
-  char *attrname = kdbGetString(aname);
   if(H5Fis_hdf5(filename) <= 0){
     free(filename);
     return krr((S)"file does not exist or is not HDF5");
   }
   file = H5Fopen(filename, H5F_ACC_RDWR, H5P_DEFAULT);
+  char *dataname = kdbGetString(dname);
   data = openGroupData(file, dataname);
   if(data < 0){
     closeGroupData(file, dataname, data);
     H5Fclose(file);
     free(filename);
     free(dataname);
-    free(attrname);
     return krr((S)"group/datset does not exist");
   }
+  char *attrname = kdbGetString(aname);
   if(H5Adelete(data,attrname) < 0)
     krr((S)"attribute could not be deleted");
   closeGroupData(file, dataname, data);
