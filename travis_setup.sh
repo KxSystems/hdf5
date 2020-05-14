8f5f088038e6969df1cada9a714df3ac60827c58
nmcdonnell-kx <nmcdonnell@kx.com>
2020-05-14 18:49:51 +0100

    Building hdf5 from source

--- travis_setup.sh
+++ travis_setup.sh
@@ -9,7 +9,17 @@ else # install from source
   wget "https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-1.10.4/src/hdf5-1.10.4.tar.gz"
   tar -xzf "hdf5-1.10.4.tar.gz"
   cd "hdf5-1.10.4"
-  ./configure --prefix=/usr/local
-  sudo make install 2>&1 | tail -n200
+  if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+    ./configure --prefix=/usr/local
+    sudo make install 2>&1 | tail -n200
+  else
+    # Windows build
+    mkdir cmake && cd cmake
+    mkdir install
+    export HDF5_HOME="${pwd}/install"
+    cmake -DCMAKE_INSTALL_PREFIX="${HDF5_HOME}" ..
+    cmake --build . --config Release
+    cmake --install . --config Release
+  fi
   cd ..
 fi
