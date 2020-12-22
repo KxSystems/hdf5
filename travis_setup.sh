7b0fa22dfce3ced53dd0751437bc90a0bc9f7816
mshimizu-kx <mshimizu@glory.firstderivatives.com>
2020-12-23 02:19:57 +0900

    Changed BUILD_HOME to HDF5_INSTALL_DIR in travis

--- travis_setup.sh
+++ travis_setup.sh
@@ -8,15 +8,15 @@ else # install from source
   wget --no-check-certificate "https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-1.10.4/src/hdf5-1.10.4.tar.gz"
   tar -xzf "hdf5-1.10.4.tar.gz"
   cd "hdf5-1.10.4"
-  export BUILD_HOME=$TRAVIS_BUILD_DIR/cbuild
-  mkdir $BUILD_HOME
+  export HDF5_INSTALL_DIR=$TRAVIS_BUILD_DIR/cbuild
+  mkdir $HDF5_INSTALL_DIR
   if [ "$TRAVIS_OS_NAME" == "linux" ]; then
-    ./configure --prefix=$BUILD_HOME
+    ./configure --prefix=$HDF5_INSTALL_DIR
     sudo make install 2>&1 | tail -n200
   else
     # Windows build
     mkdir cmake && cd cmake
-    cmake -G "Visual Studio 15 2017 Win64" -DCMAKE_INSTALL_PREFIX=$BUILD_HOME ..
+    cmake -G "Visual Studio 15 2017 Win64" -DCMAKE_INSTALL_PREFIX=$HDF5_INSTALL_DIR ..
     cmake --build . --config Release
     cmake --build . --config Release --target install
     cd ..
